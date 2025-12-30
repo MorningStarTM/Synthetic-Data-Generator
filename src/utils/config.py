30d0828098ffc6f47c2c1740624d7ea9f73c87b0
ErnestBeckham <ernestbeckham1998@gmail.com>
2025-12-30 20:22:32 +0530

    config updated with parameters

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,3 +1,12 @@
 config = {
+    "provider": {
+        "model": "ollama/gemma3:4b",
+        "lm_kwargs": {"max_tokens": 4096},
+        "qa_template_path": "src\\template\\prompt_template.txt",
+    },
+    "context_dir": "src\\context",
     "default_num_questions": 2,
+    "example_format":{'question':'{question}','answer':'{answer}'},
+    "output_dir": "src\\output"
+
 }
