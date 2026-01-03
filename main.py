20c2ca6a6ebdd90ce0d5bf7049c55aa023ca2451
ErnestBeckham <ernestbeckham1998@gmail.com>
2026-01-03 13:00:43 +0530

    updated

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 import dspy
 from src.core.template_registry import TemplateRegistry  
 from src.core.qa_generator import QAGenerator
+from src.utils.config import config
+import pandas as pd
 
-
-config = {
-    "provider": {
-        "model": "ollama/gemma3:4b",
-        "lm_kwargs": {"max_tokens": 4096},
-        "qa_template_path": "src\\template\\prompt_template.txt",
-    },
-    "context_dir": "src\\context",
-    "default_num_questions": 2,
-    "example_format":{'question':'{question}','answer':'{answer}'},
-    "output_dir": "src\\output"
-
-}
 
 
 qa_gen = QAGenerator(config)
-output_json = qa_gen.generate("physics.txt", num_sample=2, prompt_optimization=False)
-print(output_json)
+output = qa_gen.generate_sensorial_data("sample_data.txt", num_sample=2)
+print(output)
