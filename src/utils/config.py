13e039600a5f2e2ffa4d2dfacc4dd2a961da453c
ErnestBeckham <ernestbeckham1998@gmail.com>
2026-01-03 13:00:27 +0530

    updated

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,3 +1,6 @@
+from src.utils.utils import read_txt
+from pathlib import Path
+
 config = {
     "provider": {
         "model": "ollama/gemma3:4b",
@@ -5,8 +8,25 @@ config = {
         "qa_template_path": "src\\template\\prompt_template.txt",
     },
     "context_dir": "src\\context",
-    "default_num_questions": 2,
-    "example_format":{'question':'{question}','answer':'{answer}'},
-    "output_dir": "src\\output"
+    "default_num_samples": 2,
+    "example_format":   read_txt(Path("src\context\sample_data.txt")),
+    "output_dir": "output",
+    "filename":"sythentic.csv",
+    "additional_prompts":read_txt(Path("src/template/additional_element/additional_prompt.txt")),
+    'expected_schema':{
+                        "PlayerID": "int",
+                        "Age": "int",
+                        "Gender": "str",
+                        "Location": "str",
+                        "GameGenre": "str",
+                        "PlayTimeHours": "float",
+                        "InGamePurchases": "int",
+                        "GameDifficulty": "str",
+                        "SessionsPerWeek": "int",
+                        "AvgSessionDurationMinutes": "int",
+                        "PlayerLevel": "int",
+                        "AchievementsUnlocked": "int",
+                        "EngagementLevel": "str",
+                    }
 
 }
