935154408a5987aa2d7b8e489bf4ede88abb464d
ErnestBeckham <ernestbeckham1998@gmail.com>
2026-01-10 19:02:05 +0530

    additional prompt rendering included

--- src/core/model_signature.py
+++ src/core/model_signature.py
@@ -6,10 +6,25 @@ from src.core.processor import QAPromptProcessor
 import dspy
 from src.utils.config import config
 from src.core.template_registry import TemplateRegistry
+from src.core.stats_extractor import TimeSeriesStatsExtractor, ExtractorConfig
 from src.utils.logger import logger
 
 
+
+cfg = ExtractorConfig(
+        detail="lite",               # "lite" for LLM prompts, "full" for debugging
+        include_categorical=False,    # set True if you want categorical stats too
+        resample_rule=None,           # set e.g. "1D" if needed
+        fill_method="ffill",
+        compute_cross_corr=True,
+        compute_cross_lagged=True,
+    )
+
+
 processor = QAPromptProcessor(config)
+extractor = TimeSeriesStatsExtractor(cfg)
+
+
 
 def load_prompt_template(template_path: str) -> str:
     """Load a prompt template from a text file."""
@@ -31,9 +46,17 @@ def build_qa_signature_from_latest_template(
         use_user_dir=False,
         name_prefix=name_prefix,
     )
-
+    if config['task_description']:
+        stats = extractor.from_csv(config['time_series_path'])
+        stats_json = extractor.to_prompt_json(stats)
+        ts_prompt_text = load_prompt_template(config['time_series_data_path'])
+        additional_prompt = processor.render_additional_prompts(
+                                                    ts_prompt_text,
+                                                    strict=True,
+                                                    stats_placeholder=stats_json
+                                                )
     raw_prompt_text = load_prompt_template(template_path)
-    prompt_text = processor.render(raw_prompt_text)
+    prompt_text = processor.render(raw_prompt_text, None, additional_prompt)
     logger.info(f"prompt:\n  ===============================\n\n{prompt_text}\n\n==============================")
 
 
