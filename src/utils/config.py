32eb4c23cfdb318c0c0a9052acb7866212918cf0
ErnestBeckham <ernestbeckham1998@gmail.com>
2026-01-12 13:28:40 +0530

    params added

--- src/utils/config.py
+++ src/utils/config.py
@@ -2,17 +2,21 @@ from src.utils.utils import read_txt
 from pathlib import Path
 
 config = {
+    
     "provider": {
+        "hoster":"ollama",
         "model": "ollama/gemma3:4b",
         "lm_kwargs": {"max_tokens": 4096},
         "qa_template_path": "src\\template\\prompt_template.txt",
     },
+    
     "context_dir": "src\\context",
     "default_num_samples": 2,
-    "example_format":   read_txt(Path("src\context\sample_data.txt")),
+    "example_format":   Path("src\\context\\Month_Value.csv"),
     "output_dir": "output",
     "filename":"sythentic.csv",
     "additional_prompts":read_txt(Path("src/template/additional_element/additional_prompt.txt")),
+    "additional_prompt":Path("src\\template\\additional_element\\additional_prompt.txt"),
     'expected_schema':{
                         "PlayerID": "int",
                         "Age": "int",
@@ -28,7 +32,8 @@ config = {
                         "AchievementsUnlocked": "int",
                         "EngagementLevel": "str",
                     },
-    'task_description':'time-series',
-    'time_series_data_path':Path('src/template/additional_element/time_series_prompt.txt')
+    'task_description':"time-series", #'imbalance class data generation',
+    'time_series_data_path':Path('src/template/additional_element/time_series_prompt.txt'),
+    'time_series_path':Path("src\\context\\Month_Value.csv")
 
 }
